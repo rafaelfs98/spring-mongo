b30d48854714079dc6cb4513761cd14867c5fb3b
rafaelfs98 <rafaelfs98@outlook.com>
2023-03-20 10:59:58 -0300

    spring-mongo

---
+++ src/main/java/com/rfs/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.rfs.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rfs.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
